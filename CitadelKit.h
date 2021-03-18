12b6a88364107b303ca1b46398ba3d7164ce3fa0
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-03-18 01:26:37 +0100

    Extracting CitadelKit as a separate library

---
+++ CitadelKit.h
@@ -0,0 +1,19 @@
+//
+//  CitadelKit.h
+//  CitadelKit
+//
+//  Created by Maxim Orlovsky on 1/31/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "citadel.h"
+
+//! Project version number for CitadelKit.
+FOUNDATION_EXPORT double CitadelKitVersionNumber;
+
+//! Project version string for CitadelKit.
+FOUNDATION_EXPORT const unsigned char CitadelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CitadelKit/PublicHeader.h>
+
+
